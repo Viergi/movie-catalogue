ec27afd02020f0e77421f3495ff141eacfe2bc0d
Viergi <viergiansoka@gmail.com>
2024-03-28 20:46:50 +0700

    Initialize movie catalogue automated testing implemented

---
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -0,0 +1,41 @@
+/* eslint-disable implicit-arrow-linebreak */
+// const _showFoundMovies = (movies, view) => {
+//   view.showMovies(movies);
+// };
+
+class FavoriteMovieSearchPresenter {
+  constructor({ favoriteMovies, view }) {
+    this._favoriteMovies = favoriteMovies;
+    this._view = view;
+
+    this._listenToSearchRequestByUser();
+  }
+
+  _listenToSearchRequestByUser() {
+    this._view.runWhenUserIsSearching((latestQuery) => {
+      this._searchMovies(latestQuery);
+    });
+  }
+
+  async _searchMovies(latestQuery) {
+    this._latestQuery = latestQuery.trim();
+    let foundMovies;
+    if (this.latestQuery.length > 0) {
+      foundMovies = await this._favoriteMovies.searchMovies(this.latestQuery);
+    } else {
+      foundMovies = await this._favoriteMovies.getAllMovies();
+    }
+
+    const _showFoundMovies = (movies) => {
+      this._view.showFavoriteMovies(movies);
+    };
+
+    _showFoundMovies(foundMovies);
+  }
+
+  get latestQuery() {
+    return this._latestQuery;
+  }
+}
+
+export default FavoriteMovieSearchPresenter;
